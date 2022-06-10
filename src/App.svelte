b947772b7fae78fc5b2a17d1b76b299c6368b28a
flavien-hugs <geekactux@gmail.com>
2022-06-10 03:01:58 +0000

    add component Note on home page

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
-  import Home from '@/pages/Home.svelte'
-  import Header from '@/components/Header.svelte'
+    // pages
+    import Home from '@/pages/Home.svelte'
+
+    // components
+    import Note from '@/components/Note.svelte'
+    import Header from '@/components/Header.svelte'
 </script>
 
 <main>
-  <Header/>
-
-  <Home/>
+    <Header/>
+    <Note
+        id={1234}
+        title={'Some note'}
+        content={'This note is about this that'}
+        tags={['Test', 'Text']}
+        date={'20220610000'}
+        isFavorite={false}
+    />
+    <Home/>
 </main>
 
 <style lang="scss">
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    height: 100%;
-  }
-  :global(body) {
-    margin: 0;
-    height: 100%;
-    background-color: #fbfbfb;
-  }
+    :root {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        height: 100%;
+    }
+    :global(body) {
+        margin: 0;
+        height: 100%;
+        background-color: #fbfbfb;
+    }
 </style>
